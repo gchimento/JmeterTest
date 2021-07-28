95335d27c98bddf32cd2776660beaafa1aa2ba27
Matiassn97 <83656721+Matiassn97@users.noreply.github.com>
2021-07-28 09:39:47 -0300

    Script v2

--- passfail.sh
+++ passfail.sh
@@ -1,2 +1,9 @@
 #!/bin/bash
-echo 
+RT_AVG=$1
+PER95=$2
+
+echo "reporting: 
+- module: passfail 
+  criteria: 
+  - avg-rt>"$RT_AVG"ms, continue as failed 
+  - p95>"$PER95"ms, continue as failed">passfail.yaml
