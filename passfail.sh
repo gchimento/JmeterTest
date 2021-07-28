442591834bb7e88e1937ba14f70ff34855ae4cbf
Matiassn97 <83656721+Matiassn97@users.noreply.github.com>
2021-07-28 10:32:12 -0300

    Update passfail.sh

--- passfail.sh
+++ passfail.sh
@@ -1,15 +1,18 @@
 #!/bin/bash
 RT_AVG=$1
 PER95=$2
-REQ=$3
+PERERR=$3
+REQ=$4
 if [ -z $REQ ];
 then echo "reporting: 
 - module: passfail 
   criteria: 
     Se supero el tiempo de respuesta promedio maximo ("$RT_AVG"): avg-rt>"$RT_AVG"ms, continue as failed 
-    Se supero el tiempo maximo del 95 percentil ("$PER95"): p95>"$PER95"ms, continue as failed">passfail.yaml; 
+    Se supero el tiempo maximo del 95 percentil ("$PER95"): p95>"$PER95"ms, continue as failed
+    Se supero el procentaje maximo de errores ("$PERERR"): fail>"$PERERR"%, continue as failed">passfail.yaml; 
 else echo "reporting: 
 - module: passfail 
   criteria: 
     Se supero el tiempo de respuesta promedio maximo ("$RT_AVG"): avg-rt of $REQ>"$RT_AVG"ms, continue as failed 
-    Se supero el tiempo maximo del 95 percentil ("$PER95"): p95 of $REQ>"$PER95"ms, continue as failed">passfail.yaml; fi
+    Se supero el tiempo maximo del 95 percentil ("$PER95"): p95 of $REQ>"$PER95"ms, continue as failed
+    Se supero el procentaje maximo de errores ("$PERERR"): fail>"$PERERR"%, continue as failed">passfail.yaml; fi
