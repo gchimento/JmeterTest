a06596fdde8438b42d471bbf69c76dc5463c803a
Matiassn97 <83656721+Matiassn97@users.noreply.github.com>
2021-08-02 11:10:22 -0300

    update

---
+++  jmeter-demo-param-v2/passfail.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+RT_AVG=$1
+PER95=$2
+HITS=$3
+PERERR=$4
+REQ=$5
+if [ -z $REQ ];
+then echo "reporting: 
+- module: passfail 
+  criteria: 
+    Se supero el tiempo de respuesta promedio maximo ("$RT_AVG"ms): avg-rt>"$RT_AVG"ms, continue as failed 
+    Se supero el tiempo maximo del 95 percentil ("$PER95"ms): p95>"$PER95"ms, continue as failed
+    No se llego a la cantidad especificada de requests ("$HITS"): hits<"$HITS", continue as failed
+    Se supero el procentaje maximo de errores ("$PERERR"%): fail>"$PERERR"%, continue as failed">passfail.yaml; 
+else echo "reporting: 
+- module: passfail 
+  criteria: 
+    Se supero el tiempo de respuesta promedio maximo ("$RT_AVG"ms): avg-rt of $REQ>"$RT_AVG"ms, continue as failed 
+    Se supero el tiempo maximo del 95 percentil ("$PER95"ms): p95 of $REQ>"$PER95"ms, continue as failed
+    No se llego a la cantidad especificada de requests ("$HITS"): hits of $REQ<"$HITS", continue as failed    
+    Se supero el procentaje maximo de errores ("$PERERR"%): fail>"$PERERR"%, continue as failed">passfail.yaml; fi
