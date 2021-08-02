1690f38f925333ea98cc3e7ca07e15d320802cc3
Matiassn97 <83656721+Matiassn97@users.noreply.github.com>
2021-08-02 17:14:21 -0300

    new

---
+++ jmeter-test-resp-seg/execution.bat
@@ -0,0 +1,23 @@
+@set JMX=%1
+@set DATA=%2
+@if not defined DATA (
+	@echo execution: >execution.yaml
+	@echo - concurrency: {} >>execution.yaml
+	@echo   executor: jmeter>>execution.yaml
+	@echo   scenario: %JMX%>>execution.yaml
+	@echo   throughput: {}>>execution.yaml
+	@echo scenarios: >>execution.yaml
+	@echo   %JMX%: >>execution.yaml
+	@echo     script: %cd%\%JMX%.jmx>>execution.yaml
+) else (
+	@echo execution: >execution.yaml
+	@echo - concurrency: {} >>execution.yaml
+	@echo   executor: jmeter>>execution.yaml
+	@echo   scenario: %JMX%>>execution.yaml
+	@echo   throughput: {}>>execution.yaml
+	@echo scenarios: >>execution.yaml
+	@echo   %JMX%: >>execution.yaml
+	@echo     script: %cd%\%JMX%.jmx>>execution.yaml
+	@echo     data-sources:>>execution.yaml
+	@echo     - path: %cd%\%DATA%.csv>>execution.yaml
+)
