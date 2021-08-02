2642060f87cb25439ff84449e158b6e918642ce9
Matiassn97 <83656721+Matiassn97@users.noreply.github.com>
2021-08-02 18:00:27 -0300

    Update execution.bat

--- jmeter-test-resp-seg/execution.bat
+++ jmeter-test-resp-seg/execution.bat
@@ -9,6 +9,10 @@
 	@echo scenarios: >>execution.yaml
 	@echo   %JMX%: >>execution.yaml
 	@echo     script: %cd%\%JMX%.jmx>>execution.yaml
+	@echo     modifications: >>execution.yaml
+	@echo       set-prop: >>execution.yaml
+	@echo         "Test Plan>TestPlan.serialize_threadgroups": true >>execution.yaml
+	
 ) else (
 	@echo execution: >execution.yaml
 	@echo - concurrency: {} >>execution.yaml
@@ -18,6 +22,9 @@
 	@echo scenarios: >>execution.yaml
 	@echo   %JMX%: >>execution.yaml
 	@echo     script: %cd%\%JMX%.jmx>>execution.yaml
+	@echo     modifications: >>execution.yaml
+	@echo       set-prop: >>execution.yaml
+	@echo         "Test Plan>TestPlan.serialize_threadgroups": true >>execution.yaml
 	@echo     data-sources:>>execution.yaml
 	@echo     - path: %cd%\%DATA%.csv>>execution.yaml
 )
