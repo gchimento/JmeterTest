5fbc369dfedfc89b2888f06fc1f51bc9c8e3ec56
Matiassn97 <83656721+Matiassn97@users.noreply.github.com>
2021-08-02 18:06:22 -0300

    Update execution.bat

--- jmeter-test-resp-seg/execution.bat
+++ jmeter-test-resp-seg/execution.bat
@@ -5,26 +5,25 @@
 	@echo - concurrency: {} >>execution.yaml
 	@echo   executor: jmeter>>execution.yaml
 	@echo   scenario: %JMX%>>execution.yaml
+	@echo     modifications: >>execution.yaml
+	@echo       set-prop: >>execution.yaml
+	@echo         "Test Plan>TestPlan.serialize_threadgroups": true >>execution.yaml
 	@echo   throughput: {}>>execution.yaml
 	@echo scenarios: >>execution.yaml
 	@echo   %JMX%: >>execution.yaml
 	@echo     script: %cd%\%JMX%.jmx>>execution.yaml
-	@echo     modifications: >>execution.yaml
-	@echo       set-prop: >>execution.yaml
-	@echo         "Test Plan>TestPlan.serialize_threadgroups": true >>execution.yaml
-	
 ) else (
 	@echo execution: >execution.yaml
 	@echo - concurrency: {} >>execution.yaml
 	@echo   executor: jmeter>>execution.yaml
 	@echo   scenario: %JMX%>>execution.yaml
+	@echo     modifications: >>execution.yaml
+	@echo       set-prop: >>execution.yaml
+	@echo         "Test Plan>TestPlan.serialize_threadgroups": true >>execution.yaml
 	@echo   throughput: {}>>execution.yaml
 	@echo scenarios: >>execution.yaml
 	@echo   %JMX%: >>execution.yaml
 	@echo     script: %cd%\%JMX%.jmx>>execution.yaml
-	@echo     modifications: >>execution.yaml
-	@echo       set-prop: >>execution.yaml
-	@echo         "Test Plan>TestPlan.serialize_threadgroups": true >>execution.yaml
 	@echo     data-sources:>>execution.yaml
 	@echo     - path: %cd%\%DATA%.csv>>execution.yaml
 )
