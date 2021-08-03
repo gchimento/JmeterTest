833a1fd371ac33100d73acaff3f26ad305702be0
Matiassn97 <83656721+Matiassn97@users.noreply.github.com>
2021-08-03 17:26:18 -0300

    add

---
+++ jmeter-test-resp-seg/monitoring.bat
@@ -0,0 +1,13 @@
+@set INT=%1
+@set LOG=%2
+@set PERERR=%3
+@set REQ=%4
+@echo services: >monitoring.yaml
+@echo - module: monitoring >>monitoring.yaml
+@echo     ~local: >>monitoring.yaml
+@echo     - interval: %INT%s >>monitoring.yaml
+@echo       logging: %LOG% >>monitoring.yaml
+@echo       metrics: >>monitoring.yaml
+@echo       - cpu >>monitoring.yaml
+@echo       - disk-space >>monitoring.yaml
+@echo       - engine-loop >>monitoring.yaml
--- jmeter-test-resp-seg/passfail.bat
+++ jmeter-test-resp-seg/passfail.bat
@@ -1,14 +1,11 @@
-@set RT_AVG=%1
-@set PER95=%2
-@set HITS=%3
-@set PERERR=%4
-@set REQ=%5
+@set PER95=%1
+@set HITS=%2
+@set PERERR=%3
+@set REQ=%4
 @if not defined REQ (
 	@echo reporting: >passfail.yaml
 	@echo - module: passfail >>passfail.yaml
 	@echo   criteria: >>passfail.yaml
-	echo Se setea el tiempo de respuesta promedio maximo en %RT_AVG%ms para el Total
-	@echo     Se supero el tiempo de respuesta promedio maximo (%RT_AVG%ms^): avg-rt^>%RT_AVG%ms, continue as failed >>passfail.yaml
 	echo Se setea el tiempo de percentil 95 maximo en %PER95%ms para el Total
 	@echo     Se supero el tiempo maximo del 95 percentil (%PER95%ms^): p95^>%PER95%ms, continue as failed >>passfail.yaml
 	echo Se setea el minimo de request en %HITS% para el Total
@@ -19,8 +16,6 @@
 	@echo reporting: >passfail.yaml
 	@echo - module: passfail >>passfail.yaml
 	@echo   criteria: >>passfail.yaml
-	echo Se setea el tiempo de respuesta promedio maximo en %RT_AVG%ms para el request %REQ%
-	@echo     Se supero el tiempo de respuesta promedio maximo (%RT_AVG%ms^): avg-rt of %REQ%^>%RT_AVG%ms, continue as failed >>passfail.yaml
 	echo Se setea el tiempo de percentil 95 maximo en %PER95%ms para el request %REQ%
 	@echo     Se supero el tiempo maximo del 95 percentil (%PER95%ms^): p95 of %REQ%^>%PER95%ms, continue as failed >>passfail.yaml
 	echo Se setea el minimo de request en %HITS% para el request %REQ%
