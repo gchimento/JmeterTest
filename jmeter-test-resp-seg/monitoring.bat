20c4199a7240f60041f546b9c562ad93fc16df8a
Matiassn97 <83656721+Matiassn97@users.noreply.github.com>
2021-08-03 17:39:04 -0300

    Update monitoring.bat

--- jmeter-test-resp-seg/monitoring.bat
+++ jmeter-test-resp-seg/monitoring.bat
@@ -5,8 +5,4 @@
 @echo services: >monitoring.yaml
 @echo - local: >>monitoring.yaml
 @echo     - interval: %INT%s >>monitoring.yaml
-@echo       logging: %LOG% >>monitoring.yaml
-@echo       metrics: >>monitoring.yaml
-@echo       - cpu >>monitoring.yaml
-@echo       - disk-space >>monitoring.yaml
-@echo       - engine-loop >>monitoring.yaml
+@echo     - logging: %LOG% >>monitoring.yaml
