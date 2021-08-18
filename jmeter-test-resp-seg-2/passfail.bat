cce4aebd254a6d5e8e222a4fadde3e93ee3a7d4a
Matiassn97 <83656721+Matiassn97@users.noreply.github.com>
2021-08-18 16:11:53 -0300

    Update passfail.bat

--- jmeter-test-resp-seg-2/passfail.bat
+++ jmeter-test-resp-seg-2/passfail.bat
@@ -9,7 +9,7 @@
 	echo Se setea el tiempo de percentil 95 maximo en %PER95%ms para el Total
 	@echo     Se supero el tiempo maximo del 95 percentil (%PER95%ms^): p95^>%PER95%ms, continue as failed >>passfail.yaml
 	echo Se setea el minimo de request en %HITS% para el Total
-	@echo     No se llego a la cantidad especificada de requests (%HITS%^): hits^<%HITS% within 90s, stop as failed >>passfail.yaml
+	@echo     No se llego a la cantidad especificada de requests (%HITS%^): hits^<%HITS% over 90s, stop as failed >>passfail.yaml
 	echo Se setea el porcentaje maximo de error en %PERERR%%% para el Total
 	@echo     Se supero el procentaje maximo de errores (%PERERR%%%^): fail^>%PERERR%%%, continue as failed >>passfail.yaml
 ) else (
@@ -19,7 +19,7 @@
 	echo Se setea el tiempo de percentil 95 maximo en %PER95%ms para el request %REQ%
 	@echo     Se supero el tiempo maximo del 95 percentil (%PER95%ms^): p95 of %REQ%^>%PER95%ms, continue as failed >>passfail.yaml
 	echo Se setea el minimo de request en %HITS% para el request %REQ%
-	@echo     No se llego a la cantidad especificada de requests (%HITS%^): hits of %REQ%^<%HITS% within 90s, stop as failed >>passfail.yaml
+	@echo     No se llego a la cantidad especificada de requests (%HITS%^): hits of %REQ%^<%HITS% over 90s, stop as failed >>passfail.yaml
 	echo Se setea el porcentaje maximo de error en %PERERR%%% para el Total
 	@echo     Se supero el procentaje maximo de errores (%PERERR%%%^): fail^>%PERERR%%%, continue as failed >>passfail.yaml
 )
