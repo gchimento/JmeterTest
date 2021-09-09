db9fe08efa45029029b1e8d7b34ed18097fd1848
Matiassn97 <83656721+Matiassn97@users.noreply.github.com>
2021-09-09 09:44:45 -0300

    Update passfail.sh

--- jmeter-test-resp-seg/passfail.sh
+++ jmeter-test-resp-seg/passfail.sh
@@ -19,3 +19,8 @@ else echo "reporting:
     Se supero el tiempo maximo del 95 percentil ("$PER95"ms): p95 of $REQ>"$PER95"ms, continue as failed
     No se llego a la cantidad especificada de requests ("$HITS"): hits of $REQ<"$HITS", continue as failed    
     Se supero el procentaje maximo de errores ("$PERERR"%): fail>"$PERERR"%, continue as failed">passfail.yaml; fi
+
+echo "services:
+- module: shellexec
+  post-process:
+  - "$JMETER_HOME"/bin/jmeter.bat -Jjmeter.save.saveservice.assertion_results_failure_message=false -Jjmeter.reportgenerator.overall_granularity=15000 -Jjmeter.reportgenerator.report_title=%JOB_NAME% -g ${TAURUS_ARTIFACTS_DIR}/kpi.jtl -o ${TAURUS_ARTIFACTS_DIR}/dashboard" >>passfail.yaml
